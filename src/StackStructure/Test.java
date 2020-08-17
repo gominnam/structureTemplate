3c561103213da710093a35007929cb1c66ca0be7
minjun <koso03@naver.com>
2020-08-17 15:43:15 +0900

    stack Structure. .

---
+++ src/StackStructure/Test.java
@@ -0,0 +1,19 @@
+package StackStructure;
+
+public class Test {
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<Integer>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.peak());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
+    }
+}
